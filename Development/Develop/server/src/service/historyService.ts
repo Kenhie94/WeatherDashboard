ea3200f1612748dd43218733cface91c17d8340c
Kenjy Jap <kenhiebusiness@gmail.com>
2024-10-02 15:18:37 -0700

    Define a City and HistoryService class

--- Development/Develop/server/src/service/historyService.ts
+++ Development/Develop/server/src/service/historyService.ts
@@ -1,13 +1,33 @@
-// TODO: Define a City class with name and id properties
+import fs from 'node:fs/promises';
+import { v4 as uuidv4 } from 'uuid';
+// TODO: Define a City class with name and id properties: Done
+class City {
+  name: string;
+  id: string;
 
-// TODO: Complete the HistoryService class
+  constructor(name: string, id: string) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+// TODO: Complete the HistoryService class: Done
 class HistoryService {
-  // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
-  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  // TODO: Define a read method that reads from the searchHistory.json file: Done
+  private async read() {
+    return await fs.readFile('db/db.json'), {
+      flag: 'a+',
+      encoding: 'utf8',
+    };
+  }
+  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file: Done
+  private async write(cities: City[]) {
+    return await fs.writeFile('db/db.json', JSON.stringify(cities, null, '\t'));
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   // async addCity(city: string) {}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
